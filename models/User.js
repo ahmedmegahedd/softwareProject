ebffa56f6b13cbcbf120c8528f763cf2d3704da6
Loaifarrag27 <loai.mohamed.farrag@gmail.com>
2025-03-04 00:32:40 +0200

    Added mongoose schemas for Users, Event, Booking

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePicture: { type: String, default: "default.jpg" },
+    role: { type: String, enum: ["User", "Organizer", "Admin"], default: "User" },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', UserSchema);
